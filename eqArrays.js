85e8c18d74876c35d25681658d92536b8cd5c56a
KurtSpiker <kurt.spiker@gmail.com>
2021-09-15 00:24:23 +0000

    Working but could be cleaned

---
+++ eqArrays.js
@@ -0,0 +1,14 @@
+const eqArrays = function(array1, array2) {
+  let truthCheck = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i] && array1.length === array2.length) {
+      truthCheck++;
+    }
+    else {
+      return false;
+    }
+  }
+  if (truthCheck === array1.length) {
+    return true;
+  }
+};
