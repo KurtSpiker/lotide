e1201c102c405ef8bb2833bedb49bfd9e58ccca3
KurtSpiker <kurt.spiker@gmail.com>
2021-09-21 00:53:49 +0000

    Update

--- tail.js
+++ tail.js
@@ -1,13 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const tail = function(words) {
-  let tailArray = words.splice(0);
+  let tailArray = words.slice(1);
   return tailArray;
 }
+
+module.exports = tail;
